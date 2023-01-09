55cbf8fae2b55d47d9f41e7bdadf8657b758750c
kwilli29 <kwilli29@nd.edu>
2023-01-08 22:25:31 -0600

    script tweek

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
 
-var resp = document.getElementById("response"); // find the element w/ id="response",
-// store where it was found in var resp, like a shortcut to it
-
-var likeB = document.getElementById("likeB"); // find element with id="yesB", store location in var yesB
-// in this case, id="yesB" is a button
-
-var count = 0; // counter set to 0 to help demonstrate conditionals later
-
-
-likeB.addEventListener("click", displayYes);
-// tell button to prepare for if the user clicks it
-
-function displayYes() { //html text on page is changed when button is pressed
-  resp.innerHTML = "Cool!";
-  count++;
-  console.log("yes: " + count); // console updates which btn was pressed & how many times user presses btns
-
-  if (count == 4) { // every time count equals 4, it will reset and print a new message
-    count = 0;
-    resp.innerHTML = "Make up your mind!";
-  }
-}
-
-$('.carousel').carousel();
+// $('.carousel').carousel();
 
 //////////////
 
-asynch function populate() {
+async function populate() {
   const requestURL = 'https://raw.githubusercontent.com/kwilli29/kwilli29.github.io/main/db.json';
   const request = new Request(requestURL);
 
   const response = await fetch(request);
   const posts = await response.json();
+
+  console.log(posts);
 
   populatePosts(posts);
 
